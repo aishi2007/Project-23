c32730613ef5fa08d39f04640c1ea8dbbf3dca5d
aishi2007 <72500127+aishi2007@users.noreply.github.com>
2020-11-23 16:20:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,s1Sprite;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var s1,s2,s3;
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	
+
+	var pack_options={
+		restitution:0
+	}
+	packageSprite=createSprite(width/2, 80, 10,pack_options);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
+	World.add(world, packageBody);
+	
+
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:false} );
+ 	World.add(world, ground);
+
+	s1Sprite = Bodies.rectangle(380, 630, 200, 20 , {isStatic:true} );
+	World.add(world, s1Sprite);
+	 
+	s2= createSprite(280,610,20,100);
+	s2.shapeColor='red';
+
+	s3= createSprite(488,610,20,100);
+	s3.shapeColor='red';
+
+	s1=createSprite(380,650,200,20);
+	s1.shapeColor='red';
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody, false);
+}
+}
